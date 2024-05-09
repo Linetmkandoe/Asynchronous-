f53378396a97cfc423f203137d56577db670846b
Linet Ndau <mkandoelinet@gmail.com>
2024-05-09 16:58:02 +0300

    added files

---
+++ asynchronous.js
@@ -0,0 +1,49 @@
+function delay(time){
+    return new Promise(resolve=>setTimeout(resolve,time));
+}
+async function logAfterDelay(message, delayTime){
+    await delay(delayTime);
+    console.log(message);
+}
+logAfterDelay("Shoping hours!",5000);
+
+
+async function getUserData(id){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve(`user data for ID ${id}`);
+        },6000);
+    });
+}
+async function fetchLogUser(userIds){
+    for(const id of userIds){
+        const userData = await getUserData(id);
+        console.log(userData);
+    }
+}
+
+const usersIds = [1,3,4,7,10];
+fetchLogUser(usersIds).then(()=>
+console.log(`All the users data has been checked.`));
+
+async function performTask(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(`The task was successfully`);
+        },2000);
+
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
